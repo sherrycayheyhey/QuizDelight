de2d3571b22966b79b05996be507b90e6b3410e7
sherrycayheyhey <sherrycaywood@yahoo.com>
2020-03-17 11:47:45 -0700

    Question class added.

--- app/src/main/java/com/chromsicle/quizdelight/Question.java
+++ app/src/main/java/com/chromsicle/quizdelight/Question.java
@@ -4,4 +4,73 @@ package com.chromsicle.quizdelight;
 //this is the same info that will be stored in the database
 //this is the bridge between this app and the database
 public class Question {
+    private String question;
+    private String option1;
+    private String option2;
+    private String option3;
+    private String option4;
+    //the correct answer will be saved in the form of a number
+    private int answerNr;
+
+    //empty question object will be used when querying the database
+    //then use setter methods to set the values
+    public Question() {};
+
+    //create a constructor where all 6 values are passed when an object of this question class is created
+    public Question(String question, String option1, String option2, String option3, String option4, int answerNr) {
+        this.question = question;
+        this.option1 = option1;
+        this.option2 = option2;
+        this.option3 = option3;
+        this.option4 = option4;
+        this.answerNr = answerNr;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getOption1() {
+        return option1;
+    }
+
+    public void setOption1(String option1) {
+        this.option1 = option1;
+    }
+
+    public String getOption2() {
+        return option2;
+    }
+
+    public void setOption2(String option2) {
+        this.option2 = option2;
+    }
+
+    public String getOption3() {
+        return option3;
+    }
+
+    public void setOption3(String option3) {
+        this.option3 = option3;
+    }
+
+    public String getOption4() {
+        return option4;
+    }
+
+    public void setOption4(String option4) {
+        this.option4 = option4;
+    }
+
+    public int getAnswerNr() {
+        return answerNr;
+    }
+
+    public void setAnswerNr(int answerNr) {
+        this.answerNr = answerNr;
+    }
 }

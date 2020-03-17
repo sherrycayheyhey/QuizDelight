6842d8d4987223e3c2eb3a0b46d1e5cbbe327c25
sherrycayheyhey <sherrycaywood@yahoo.com>
2020-03-17 12:03:55 -0700

    add constants class

---
+++ app/src/main/java/com/chromsicle/quizdelight/QuizContract.java
@@ -0,0 +1,32 @@
+package com.chromsicle.quizdelight;
+
+//this class only provides the constants
+
+import android.provider.BaseColumns;
+
+public final class QuizContract {
+    //final means this can't be subclassed
+    //this private constructor means we can't create an object of it, it's like an insurance policy
+    //this class is only a container for these constants
+    private QuizContract() {}
+
+
+    //BaseColumns is an interface, Ctrl + B to see the declaration
+    //BaseCoulmns provides two more constants, id and count
+    //we'll only use id for creating another column in the database which will store the id
+    //this id constant could have been created like the ones below but some Android framework classes
+    //need this exact naming convention
+    public static class QuestionsTable implements BaseColumns {
+        //public because I want to access them outside this class
+        //static because I want to access them without needing an instance of this QuestionsTable
+        //final because I don't want to change them
+        public static final String TABLE_NAME = "quiz_questions";
+        public static final String COLUMN_QUESTION = "question";
+        public static final String COLUMN_OPTION1 = "option1";
+        public static final String COLUMN_OPTION2 = "option2";
+        public static final String COLUMN_OPTION3 = "option3";
+        public static final String COLUMN_OPTION4 = "option4";
+        public static final String COLUMN_ANSWER_NR = "answer_nr";
+    }
+
+}
